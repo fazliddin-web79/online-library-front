f96e843f5da46799227dd3f95b14e22811e31dc2
fazliddin-web79 <yakubjonovfazliddin777@gmail.com>
2023-04-16 06:59:20 +0500

    first commit

--- src/MainUrl/mainUrl.js
+++ src/MainUrl/mainUrl.js
@@ -1,2 +1,2 @@
-export const mainUrl = "http://localhost:8800";
-// export const mainUrl = "https://online-library-portifolio.onrender.com";
+// export const mainUrl = "http://localhost:8800";
+export const mainUrl = "https://online-library-portifolio.onrender.com";
